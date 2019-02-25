1c09cd77064ab7534813faa30b5d0680396dff42
Greg Walsh <walshg3@go.stockton.edu>
2019-02-25 14:08:42 -0500

    changes to wget

--- script.bash
+++ script.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd '/Users/gregwalsh/Github/BackgroundChanger/photos'
-rm 2880x1800.png
-wget --show-progress https://source.unsplash.com/random/2880x1800.png 
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/gregwalsh/Github/BackgroundChanger/photos/2880X1800.png'";
+rm newpic.png
+wget https://source.unsplash.com/2880x1800/?nature,wallpapers,architecture -O newpic.png
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/gregwalsh/Github/BackgroundChanger/photos/newpic.png'";
 killall Dock;
