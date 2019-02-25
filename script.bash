64b798799ce793a6c4416842c7a7e581c3707ec9
Greg Walsh <walshg3@go.stockton.edu>
2019-02-25 01:17:53 -0500

    Added script and photos folder

---
+++ script.bash
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+cd '/Users/gregwalsh/Github/BackgroundChanger/photos'
+rm 2880x1800.png
+wget https://source.unsplash.com/random/2880x1800.png 
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/gregwalsh/Github/BackgroundChanger/photos/2880X1800.png'";
+killall Dock;
