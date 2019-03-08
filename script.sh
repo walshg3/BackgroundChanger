ed05fccd62e90dd1de505c71f8f5553e66abba62
Greg Walsh <walshg3@go.stockton.edu>
2019-03-08 18:12:29 -0500

    Small changes including removing requirements to create a new folder
    Current problems is running with launchd. It will launch and reload dock however does not look like it pulls a new picture.

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 cd '/Users/gregwalsh/Github/BackgroundChanger/photos'
-rm newpic.png
 wget https://source.unsplash.com/2880x1800/?nature,wallpapers,architecture -O newpic.png
-sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/gregwalsh/Github/BackgroundChanger/photos/newpic.png'";
+sqlite3 ~/Library/Application\ Support/Dock/desktoppicture.db "update data set value = '/Users/gregwalsh/Github/BackgroundChanger/newpic.png'";
 killall Dock;
